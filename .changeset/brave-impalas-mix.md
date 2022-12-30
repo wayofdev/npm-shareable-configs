a97d705b261e23e2ba135dae191b710d54e7e05b
lotyp <lotyp7@gmail.com>
2022-12-30 20:27:57 +0200

    feat: updating configs

---
+++ .changeset/brave-impalas-mix.md
@@ -0,0 +1,6 @@
+---
+'@wayofdev/commitlint-config': minor
+'@wayofdev/tsconfig-config': minor
+---
+
+feat: allow js, stricter settings for commitlint
--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -8,4 +8,41 @@ module.exports = {
       https://github.com/semantic-release/git/issues/331
     */
     ignores: [commitMessage => automaticCommitPattern.test(commitMessage)],
+    rules: {
+        'body-leading-blank': [1, 'always'],
+        'body-max-line-length': [2, 'always', 100],
+        'footer-leading-blank': [1, 'always'],
+        'footer-max-line-length': [2, 'always', 100],
+        'header-max-length': [2, 'always', 100],
+        'scope-case': [2, 'always', 'lower-case'],
+        'subject-case': [
+            2,
+            'never',
+            ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+        ],
+        'subject-empty': [2, 'never'],
+        'subject-full-stop': [2, 'never', '.'],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'perf',
+                'docs',
+                'style',
+                'deps',
+                'refactor',
+                'ci',
+                'test',
+                'revert',
+                'build',
+                'chore',
+                'translation',
+                'security',
+            ],
+        ],
+    }
 };
